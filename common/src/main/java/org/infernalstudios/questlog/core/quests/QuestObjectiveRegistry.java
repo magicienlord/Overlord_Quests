08f083aa30f5b0f6cb7ed52f41087debc8db89b9
magicienlord <126299567+magicienlord@users.noreply.github.com>
2026-09-12 16:55:58 +0200

    Register tagged entity kill history objective

--- common/src/main/java/org/infernalstudios/questlog/core/quests/QuestObjectiveRegistry.java
+++ common/src/main/java/org/infernalstudios/questlog/core/quests/QuestObjectiveRegistry.java
@@ -48,6 +48,8 @@ public class QuestObjectiveRegistry {
                 new EditorMetadata("entity", "Target Entity ID:", "required_amount", EditorMetadata.SuggestionType.ENTITY_TYPE));
         register(new ResourceLocation("questlog", "entity_kill"), EntityKillObjective::new,
                 new EditorMetadata("entity", "Entity ID:", "required_amount", EditorMetadata.SuggestionType.ENTITY_TYPE));
+        register(new ResourceLocation("questlog", "entity_kill_history"), EntityKillHistoryObjective::new,
+                new EditorMetadata("entity", "Entity ID / Matcher:", null, EditorMetadata.SuggestionType.ENTITY_TYPE));
         register(new ResourceLocation("questlog", "entity_kill_stat"), EntityKillStatObjective::new,
                 new EditorMetadata("entity", "Exact Entity ID:", "required_amount", EditorMetadata.SuggestionType.ENTITY_TYPE));
         register(new ResourceLocation("questlog", "entity_approach"), EntityApproachObjective::new,
